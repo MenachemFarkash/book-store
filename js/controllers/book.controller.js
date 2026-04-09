785c508fbf423e73bc1365d01eb91e72c15adba6
Menachem Farkash <117067641+MenachemFarkash@users.noreply.github.com>
2026-04-09 22:26:27 +0300

    Books are now dinamicley render in the table

--- js/controllers/book.controller.js
+++ js/controllers/book.controller.js
@@ -1,1 +1,24 @@
-function onInit() {}
+function onInit() {
+    renderBooks()
+}
+
+function renderBooks() {
+    let booksHtml = ''
+    const booksArr = getBooks()
+    booksArr.map((book) => {
+        booksHtml += `
+        <tr>
+        <td>${book.title}</td>
+        <td>${book.price}</td>
+        <td>
+           <button>Read</button>
+           <button>Update</button>
+           <button>Delete</button>
+        </td>
+        </tr>
+        `
+    })
+
+    const elTable = document.querySelector('tbody')
+    elTable.innerHTML = booksHtml
+}
