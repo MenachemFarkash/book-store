25b32c609d29813d711bac7a72c8746d0f7d3409
Menachem Farkash <117067641+MenachemFarkash@users.noreply.github.com>
2026-04-09 22:13:56 +0300

    Repository Init

---
+++ js/services/book.service.js
@@ -0,0 +1,24 @@
+const gBooks = [
+    {
+        id: 'bg4J78',
+        title: 'Adventure Of Lori',
+        price: 120,
+        imgUrl: 'https://example.com/Adventure-Of-Lori.jpg',
+    },
+    {
+        id: 'bg8K93',
+        title: 'World Atlas',
+        price: 120,
+        imgUrl: 'https://example.com/great-gatsby.jpg',
+    },
+    {
+        id: 'bg1D93',
+        title: 'Zobra the Greek',
+        price: 120,
+        imgUrl: 'https://example.com/great-gatsby.jpg',
+    },
+]
+
+function getBooks() {
+    return gBooks
+}
---
+++ js/store.controller.js
@@ -0,0 +1,6 @@
+const book = {
+    id: 'bg4J78',
+    title: 'The Great Gatsby',
+    price: 120,
+    imgUrl: 'https://example.com/great-gatsby.jpg',
+}
